4a87c2fd9109c6ad8d71c78f1acb13001d9edcde
Thalita <thalimascarenhas@gmail.com>
2024-08-23 18:21:47 -0300

    iniciando projeto

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+function createSpace(quant){
+    if (quant >= 60) {
+        for (i = 1; i <= 60; i++){
+            let containerBools = document.querySelector('.container-bools')
+            let newBool = document.createElement('div')
+            newBool.classList.add('bool')
+            containerBools.appendChild(newBoll)
+
+            /*criar texto e colocar texto, o texto é o i, pois é o número que corresponde a bola criada*/
+        }
+    }
+}

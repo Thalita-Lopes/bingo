32e13e273006f5aac7ac6040c42c76c9ce5c45e0
Thalita <thalimascarenhas@gmail.com>
2024-09-14 19:51:19 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 
-
-
-function createSpace(quant){
-    if (quant >= 60) {
-        for (i = 1; i <= 60; i++){
+function createSpace(){
+    let quant = 10;
+    console.log(quant)
+    if (quant <= 60) {
+        
+        for (let i = 1; i <= quant; i++){
             let containerBools = document.querySelector('.container-bools')
             let newBool = document.createElement('div')
             newBool.classList.add('bool')
-            containerBools.appendChild(newBoll)
-
-            /*criar texto e colocar texto, o texto é o i, pois é o número que corresponde a bola criada*/
+            let number = document.createElement('p')
+            number.classList.add('bool-number')
+            let texto = document.createTextNode(`${i}`) 
+            containerBools.appendChild(newBool)
+            number.appendChild(texto)
         }
+        
+    } else {
+        console.log("Quantidade não permitida")
     }
+
 }
+
+function outraFuncao(){
+    window.location.href = "sorteio.html"
+}
+
+function Enviar(){
+    
+    let quant = document.getElementById("quantidade")
+    console.log(quant.value)
+    alert(`Quantidade enviada!${quant.value}`)
+    outraFuncao()
+}
+
+/*
+const iniciar = document.querySelector(".button-start")
+
+iniciar.addEventListener("click", () => {
+    window.location.href = "sorteio.html"
+})
+*/
